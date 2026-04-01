82e3a704a3ab66182c81a1e3423ce18c88d5b64e
aditiiyer <iyera@mskcc.org>
2026-04-01 16:15:56 -0400

    Updated input image size

--- bash_run_SMIT_Segmentation.sh
+++ bash_run_SMIT_Segmentation.sh
@@ -9,9 +9,6 @@
 
 
 
-#Use SMIT
-use_smit=1 #Use SMIT not SMIT+
-
 #Data folder and there need a 'data.json' file in the folder 
 data_dir="$1"
 
@@ -26,10 +23,10 @@ python utils/gen_data_json.py $data_dir
 
 python run_segmentation_rectum.py \
     --data_dir $data_dir \
-    --load_weight_name $load_weight_name \
-    --save_folder $save_folder \
+    --pretrained_model_path $load_weight_name \
+    --results_dir $save_folder \    
     --out_channels 2 \
     --a_min 0 --a_max 1000 \
     --space_x 1.5 --space_y 1.5 --space_z 2.0 \
-    --roi_x 128 --roi_y 128 --roi_z 128 --use_smit $use_smit
+    --roi_x 128 --roi_y 128 --roi_z 64
 
